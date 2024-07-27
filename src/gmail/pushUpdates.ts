93e2ce9c75365b63b4fbcfc4af428f776661c21f
Nikita <hormold@gmail.com>
2024-07-26 17:03:01 -0700

    Rebuild core logic for sending to multiple chats

--- src/gmail/pushUpdates.ts
+++ src/gmail/pushUpdates.ts
@@ -110,89 +110,97 @@ export const googlePushEndpoint = async (req, res) => {
       `All checks passed for ${sanitizedEmail}, now processing emails... (${emails.length} to ${user.chatsId.length} chats)`
     );
 
-    await Promise.all(
-      user.chatsId.map(async (chatId) => {
-        for (const email of emails) {
-          const cacheKey = `${emailAddress}:${email.id}:${chatId}`;
-          const rateLimit = await getValue(cacheKey);
-          if (rateLimit) {
-            warning(`Rate limit for ${cacheKey}`);
-            continue;
-          }
-          await setValue(cacheKey, true, new Date(Date.now() + 5 * 60 * 1000));
+    for (const email of emails) {
+      const cacheKey = `${emailAddress}:${email.id}`;
+      const rateLimit = await getValue(cacheKey);
+      if (rateLimit) {
+        warning(`Rate limit for ${cacheKey}`);
+        continue;
+      }
+      await setValue(cacheKey, true, new Date(Date.now() + 5 * 60 * 1000));
 
-          // Updata basic data
-          await UpdateBasicData(emailHistoryObject as IEmailHistory, email);
-          // Blacklist check
-          if (
-            user.blackListEmails &&
-            user.blackListEmails
-              .map((email) => email.toLowerCase())
-              .includes(extractEmail(email.from.trim().toLowerCase()))
-          ) {
-            await NotProcessEmail(
-              emailHistoryObject as IEmailHistory,
-              "Blacklisted sender"
-            );
-            continue;
-          }
+      // Updata basic data
+      await UpdateBasicData(emailHistoryObject as IEmailHistory, email);
+      // Blacklist check
+      if (
+        user.blackListEmails &&
+        user.blackListEmails
+          .map((email) => email.toLowerCase())
+          .includes(extractEmail(email.from.trim().toLowerCase()))
+      ) {
+        await NotProcessEmail(
+          emailHistoryObject as IEmailHistory,
+          "Blacklisted sender"
+        );
+        continue;
+      }
 
-          if (
-            extractEmail(email.from.trim().toLowerCase()) === sanitizedEmail
-          ) {
-            await NotProcessEmail(
-              emailHistoryObject as IEmailHistory,
-              "Email from self"
-            );
-            continue;
-          }
+      if (extractEmail(email.from.trim().toLowerCase()) === sanitizedEmail) {
+        await NotProcessEmail(
+          emailHistoryObject as IEmailHistory,
+          "Email from self"
+        );
+        continue;
+      }
 
-          try {
-            const analysis = await analyzeEmail(email, user, 0);
+      if (!user.isTrial && user.subscription.isActive === false) {
+        warning(`User ${sanitizedEmail} is not subscribed`);
+        await NotProcessEmail(
+          emailHistoryObject as IEmailHistory,
+          "User is not subscribed"
+        );
+        await bot.telegram.sendMessage(
+          user.telegramID,
+          `We got an email for ${sanitizedEmail}, but you are not subscribed. Please renew your subscription to continue receiving emails: /subscribe`
+        );
+        return; // Exit the loop
+      }
 
-            if (+analysis.importance === 0) {
-              await NotProcessEmail(
-                emailHistoryObject as IEmailHistory,
-                "Email is not important",
-                analysis
-              );
-              continue;
-            }
+      try {
+        const analysis = await analyzeEmail(email, user, 0);
 
-            const telegramSendResult = await createTelegramMessage(
-              chatId,
-              email,
-              sanitizedEmail,
-              analysis
-            );
-
-            if (telegramSendResult.success) {
-              await UpdateEmailAnalysis(
-                email,
-                emailHistoryObject as IEmailHistory,
-                analysis,
-                telegramSendResult
-              );
-            } else {
-              await NotProcessEmail(
-                emailHistoryObject as IEmailHistory,
-                telegramSendResult.error ?? "Unknown error"
-              );
-            }
-          } catch (err: any) {
-            await sendErrorMessage(chatId, err, email);
-            warning(`Error in sending message: ${err.message}`, err);
-            await NotProcessEmail(
-              emailHistoryObject as IEmailHistory,
-              "Error in processing email: " + err?.message
-            );
-            await handleChatError(user, chatId);
-          }
+        if (+analysis.importance === 0) {
+          await NotProcessEmail(
+            emailHistoryObject as IEmailHistory,
+            "Email is not important",
+            analysis
+          );
+          continue;
         }
-      })
-    );
 
-    res.status(204).send();
+        user.chatsId.map(async (chatId) => {
+          const telegramSendResult = await createTelegramMessage(
+            chatId,
+            email,
+            sanitizedEmail,
+            analysis
+          );
+
+          if (telegramSendResult.success) {
+            await UpdateEmailAnalysis(
+              email,
+              emailHistoryObject as IEmailHistory,
+              analysis,
+              telegramSendResult
+            );
+          } else {
+            await NotProcessEmail(
+              emailHistoryObject as IEmailHistory,
+              telegramSendResult.error ?? "Unknown error"
+            );
+          }
+        });
+      } catch (err: any) {
+        await sendErrorMessage(user.telegramID, err, email);
+        warning(`Error in sending message: ${err.message}`, err);
+        await NotProcessEmail(
+          emailHistoryObject as IEmailHistory,
+          "Error in processing email: " + err?.message
+        );
+      }
+    }
+
+    res.status(204).send(`Processed ${emails.length} emails`);
   } catch (e: unknown) {
     error(
       `Error in googlePushEndpoint: ${
