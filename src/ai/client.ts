f56cd781dbd08f08c1ef66e772a8ab756683d19d
Nikita <hormold@gmail.com>
2025-01-29 08:51:33 -0800

    remove middle man, change model

--- src/ai/client.ts
+++ src/ai/client.ts
@@ -2,10 +2,10 @@ import OpenAI from "openai";
 
 const normalOpenAi = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
-  baseURL: "https://oai.helicone.ai/v1",
-  defaultHeaders: {
-    "Helicone-Auth": `Bearer ${process.env.HELICONE_API_KEY}`,
-  },
+  //baseURL: "https://oai.helicone.ai/v1",
+  //defaultHeaders: {
+  //  "Helicone-Auth": `Bearer ${process.env.HELICONE_API_KEY}`,
+  //},
 });
 
 export const openai = normalOpenAi;
